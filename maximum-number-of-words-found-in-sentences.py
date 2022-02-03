6f235066dfa931d192a5b40af2d58d84b7bfe05b
khanhvynguyen <nguyenkhanhvy1995@gmail.com>
2022-02-02 22:34:45 -0500

    first commit

---
+++ maximum-number-of-words-found-in-sentences.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/ 
+
+from typing import List
+class Solution:
+    def mostWordsFound(self, sentences: List[str]) -> int:
+        
+        new_sentences = []
+        for sentence in sentences:
+            new_sentences.append( sentence.split(" ") )
+        sorted_sentences = sorted(new_sentences, key=len)
+        longest_sentence = sorted_sentences[-1]
+        return len(longest_sentence)
+
+
+sentences = ["please wait", "continue to fight", "continue to win"]
+print(Solution().mostWordsFound(sentences))
+print("Hello Vy")
